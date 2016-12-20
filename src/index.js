ed82aed86986077e5c141cd85e4afa2c4960e618
Jake Wiesler <jakewiesler@gmail.com>
2016-12-20 13:51:47 -0500

    Add support for spacebar start/stop & add Phase count for styling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './css/styles.css';
 import Time from './js/components/Time';
 import Controls from './js/components/Controls';
-import { _25, _test } from './js/helpers';
+import Phases from './js/components/Phases';
+import { _25, _05, _test, handleOnKeyDown } from './js/helpers';
+import EventListener from 'react-event-listener';
 
 class Timer extends React.Component {
 	constructor(props) {
@@ -11,19 +13,28 @@ class Timer extends React.Component {
 
 		this.state = {
 			interval: null,
+		  step: 0,
+			phase: '',
 			timeRemaining: this.getTimeRemaining(_test)
 		}
 
-		this.handleStartTimer = this.handleStartTimer.bind(this);
+		this.handleStartTimer	= this.handleStartTimer.bind(this);
 		this.handleStopTimer  = this.handleStopTimer.bind(this);
-		this.startTimer       = this.startTimer.bind(this);
+		this.startTimer      	= this.startTimer.bind(this);
 		this.displayTime      = this.displayTime.bind(this);
 		this.getTimeRemaining = this.getTimeRemaining.bind(this);
+		this.nextPhase 				= this.nextPhase.bind(this);
+		this.handleOnKeyDown  = handleOnKeyDown.bind(this);
 	}
 
 	handleStartTimer() {
 		if (!this.state.interval) {
 			console.log('starting...');
+
+			/* Update the class of the timer to add styles depending on which step it is on */
+			this.setState({
+				phase: this.state.step % 2 === 0 ? 'active' : 'rest'
+			})
 
 			this.startTimer();
 		}
@@ -34,7 +45,7 @@ class Timer extends React.Component {
 			console.log('stopping...');
 
 			clearInterval(this.state.interval);
-			this.setState({ interval: null });
+			this.setState({ interval: null, phase: '' });
 		}
 	}
 
@@ -48,32 +59,47 @@ class Timer extends React.Component {
 	displayTime() {
 		if (this.state.timeRemaining.total > 0) {
 			console.log('inside displayTime');
-			// get time remaining
-			let timeRemaining = this.getTimeRemaining(this.state.timeRemaining.total - 1000); // time Remaining - 1 second
-
-			// set time remaining
+			/* get & set time remaining every 1 second */
+			let timeRemaining = this.getTimeRemaining(this.state.timeRemaining.total - 1000);
 			this.setState({ timeRemaining });
 		}
 		else {
-			this.handleStopTimer();
+			this.nextPhase(); // move to next phase
 		}
 
 	}
 
-	getTimeRemaining(time) {
+	getTimeRemaining(timeInMilliSecs) {
 		// return a string value of the time remaining
-		const total = time,
+		const total = timeInMilliSecs,
 					minutes = Math.floor( (total/1000/60) % 60 ),
 					seconds = Math.floor( (total/1000) % 60) < 10 ? '0' + Math.floor( (total/1000) % 60 ) : Math.floor( (total/1000) % 60 );
 
 		return { total, minutes, seconds };
 	}
 
+	nextPhase() {
+		console.log('switching to next phase...');
+
+		this.handleStopTimer();
+		let step = this.state.step + 1;
+		this.setState({
+			step,
+			timeRemaining: step % 2 === 0 ? this.getTimeRemaining(_25) : this.getTimeRemaining(_05)
+		});
+	}
+
 	render() {
 		return (
-			<div className='timer'>
-				<Time time={this.state.timeRemaining} />
-				<Controls handleStartTimer={this.handleStartTimer} handleStopTimer={this.handleStopTimer}/>
+			<div className={`container ${this.state.phase}`} >
+				<div className='timer'>
+					<Time time={this.state.timeRemaining} />
+					<Controls handleStartTimer={this.handleStartTimer} handleStopTimer={this.handleStopTimer}/>
+				</div>
+				<div className='phase-container'>
+					<Phases phase={this.state.phase}/>
+				</div>
+				<EventListener target={document} onKeyDown={this.handleOnKeyDown} />
 			</div>
 		)
 	}
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,2 +1,11 @@
+/* MINUTES to MILLISECONDS */
 export const _25 = 1500000;
+export const _05 = 300000;
 export const _test = 15000;
+
+export function handleOnKeyDown(e) {
+	if (e.key === " ") {
+		e.preventDefault();
+		this.handleStopTimer();
+	}
+}
